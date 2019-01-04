6684222742b9f7502a2a88fa3b5f6f3df6bd51de
lfarrell <farrelldlfarrell@gmail.com>
2019-01-03 19:26:57 -0500

    Update query

--- daily-files.php
+++ daily-files.php
@@ -12,7 +12,8 @@ for ($i=$days_since_jan_2000; $i>0; $i--) {
     UNION ALL
     SELECT reading_date, kbdi as kbd, tmp as temp, wind as breeze, rh as humidity, ppt as rain
     FROM state, station, weather
-    WHERE state.id = station.state_id and station.id = weather.station_id and reading_date = ?
+    WHERE state.id = station.state_id and station.id = weather.station_id
+    and kbdi >= 0 and tmp != -99 and rh != -99 and ppt != -99 and wind != -99 and reading_date = ?
     INTO OUTFILE '" . $output_path . "/station_dailies/" . $file_date . ".csv'
     FIELDS TERMINATED BY ','
     ENCLOSED BY '\"'
